9eb897cbada0f6eeba6b302f09e0738f9c199fbc
Frederic.K <frederic.kreuder@gmail.com>
2023-06-16 18:01:14 +0200

    Setting up static pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Header from './components/Header/Header'
-import Home from './pages/Home/Home'
 import Footer from './components/Footer/Footer'
+import Home from './pages/Home/Home'
 import Error404 from './pages/404/Error404'
+import Login from './pages/Login/Login'
+import Profile from './pages/Profile/Profile'
 
 function App() {
   return (
@@ -11,8 +13,8 @@ function App() {
       <Header />
       <Routes>
         <Route path="/" element={<Home />} />
-        {/* <Route path="/login" element={<Login />} />
-        <Route path="/profile" element={<Profile />} /> */}
+        <Route path="/login" element={<Login />} />
+        <Route path="/profile" element={<Profile />} />
         <Route path="*" element={<Error404 />} />
       </Routes>
       <Footer />
