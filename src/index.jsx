29838854f6b291779edb0b69d42e3d0db2d24267
Frederic.K <frederic.kreuder@gmail.com>
2023-06-19 14:41:21 +0200

    Set provider

--- src/index.jsx
+++ src/index.jsx
@@ -3,14 +3,16 @@ import ReactDOM from 'react-dom/client'
 import 'normalize.css'
 import './styles/style.scss'
 import App from './App'
-// import { Provider } from 'react-redux'
-// import { store } from './Store/store'
+import { Provider } from 'react-redux'
+import { store } from './Store/store'
 
-const root = ReactDOM.createRoot(document.getElementById('root'))
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+ReactDOM.render(
+  <Provider store={store}>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </Provider>,
+  document.getElementById('root')
 )
 
 // If you want to start measuring performance in your app, pass a function
